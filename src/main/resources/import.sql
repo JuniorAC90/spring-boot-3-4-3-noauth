a2c3efa931d2b1811c48d23c9a5a7bb798423a22
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-11-06 19:33:28 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$9gI.89Lf7Dt.l7.B4pUhCOICeEXWDO9Z1Vp9PvAaMNKiEKnT9ihpy');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$9gI.89Lf7Dt.l7.B4pUhCOICeEXWDO9Z1Vp9PvAaMNKiEKnT9ihpy');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
